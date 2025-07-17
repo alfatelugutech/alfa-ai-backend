8aef922f395500f242e472761d14ea8af83f428e
alfatelugutech <alfatelugutech@gmail.com>
2025-07-17 22:44:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -273,5 +273,5 @@ def get_stats():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
