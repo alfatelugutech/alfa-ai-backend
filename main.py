d7736576191ec24c629bb9227873c78a62ad40dc
alfatelugutech <alfatelugutech@gmail.com>
2025-07-18 00:23:30 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import pandas as pd
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    df = pd.DataFrame({"msg": ["Hello, world!"]})
-    return {"result": df.to_dict()}
